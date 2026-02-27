f067ef54f77fa1e4a808e2045adc1fe7db30d589
Miranda <553757+hsmiranda@users.noreply.github.com>
2026-02-26 21:36:13 -0300

    Feat: Update

---
+++ backend/src/StealthBackend/src/test/java/org/backend/stealth/ExampleResponseIT.java
@@ -0,0 +1,8 @@
+package org.backend.stealth;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class ExampleResponseIT extends ExampleResponseTest {
+    // Execute the same tests but in packaged mode.
+}
